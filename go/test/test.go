55f74eca1f6b99a05791741fd4afdc53f33c31a1
ashida.yuta <ashida.yuta@jp.panasonic.com>
2024-06-12 08:50:13 +0900

    add new go test

---
+++ go/test/test.go
@@ -0,0 +1,9 @@
+package test
+
+func EvenOrOdd(number int) string {
+	if number%2 == 0 {
+		return "even"
+	} else {
+		return "odd"
+	}
+}
---
+++ go/test/test_test.go
@@ -0,0 +1,25 @@
+package test
+
+import "testing"
+
+func TestEvenOrOdd(t *testing.T) {
+	result := EvenOrOdd(10)
+	if result != "even" {
+		t.Errorf("expected: even, actual: %s", result)
+	}
+}
+
+func TestEvenOrOdd2(t *testing.T) {
+	result := EvenOrOdd(9)
+	if result != "odd" {
+		t.Errorf("expected: even, actual: %s", result)
+	}
+}
+
+func TestEvenOrOdd3(t *testing.T) {
+	result := EvenOrOdd(11)
+	if result != "odd" {
+		t.Errorf("expected: even, actual: %s", result)
+
+	}
+}
